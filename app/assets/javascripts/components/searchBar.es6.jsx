f4054e7d7a0b408e8d5911c6b6aea3a813a22f2f
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2017-02-12 11:49:47 -0800

    More pane styling

---
+++ app/assets/javascripts/components/searchBar.es6.jsx
@@ -0,0 +1,9 @@
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="search" name="" value="" placeholder="Important messages" />
+      </div>
+    );
+  }
+}

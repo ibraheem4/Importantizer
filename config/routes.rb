3a290912bec44bfd77b7cfaf8372041fbc4a39c6
yusuke0324 <take.yusuke@gmail.com>
2017-02-11 16:06:06 -0800

    Impletented saving user token and use ocrrect user id and token when sending message

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :events, only: [:index]
+  root 'events#index'
+  resources :events, only: [:index, :create ]
+
+  post 'send' => 'events#send_message'
 end

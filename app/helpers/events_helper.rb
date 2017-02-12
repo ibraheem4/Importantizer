fe2675c7c81e9650e68d9bb7e813fb0dbcea74d2
Matthew Weingarten <matt@weingarten.org>
2017-02-11 22:51:15 -0800

    Adds helper methods to display the widget

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -8,4 +8,30 @@ module EventsHelper
       profile_image: response['profileImage']
      }
   end
+
+
+  def search_arr(string)
+    string.split(',').map(&:strip)
+  end
+
+  def display_with_key_words(arr)
+    output = []
+    output << key_words(arr)
+    output << Important.all
+    output.sort_by(&:created_at)
+  end
+
+  def display_without_key_words
+    Important.all.sort_by(&:created_at)
+  end
+
+
+  private
+  def key_words(arr)
+    output = []
+    arr.each do |word|
+      output << Post.all.select{|post| /#{word}/.match(post.text)}
+    end
+    output.flatten.uniq
+  end
 end

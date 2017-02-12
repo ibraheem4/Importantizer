02bed8b575d0726ce2342a8b7460b18b28bae4e2
Matthew Weingarten <matt@weingarten.org>
2017-02-11 23:57:30 -0800

    Updates posts index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
+	include PostsHelper
+	helper_method :search_params
 	def index
+		@posts = display_with_key_words(search_params)
     response.headers.except! 'X-Frame-Options'
 	end
 
@@ -11,6 +14,15 @@ class PostsController < ApplicationController
 
 		if @text == nil
 			Post.create(text: @text, user_id: @user_id)
+		end
+	end
+
+
+
+	def search_params
+		output = []
+		params.each do |key,value|
+			if key.inlcude?("search") then output << value end
 		end
 	end
 end
